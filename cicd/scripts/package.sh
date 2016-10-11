88a0afadb4ebfd8d6486e0444e5e57e309c364b6
andresfuentes <me@andresfuentes.com>
2016-10-11 11:10:58 -0400

    custom scripts

---
+++ cicd/scripts/package.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "***************************************"
+echo "***         PACKAGING BUILDER       ***"
+echo "***            1. create war        ***"
+echo "***            2. version war       ***"
+echo "***            3. create image      ***"
+echo "***            4. push image        ***"
+echo "***************************************"
+
